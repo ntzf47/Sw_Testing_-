9163816f92600991fb04ff7e8d8ecaed11d491df
ntzf47 <Supradit_p@su.ac.th>
2026-04-03 20:04:05 +0700

    Update playwright.config.js

--- Phisit650710706/playwright-tests/playwright.config.js
+++ Phisit650710706/playwright-tests/playwright.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './tests',
-  /* การตั้งค่าระบบจำลองให้มี Browser 3 แบบ แม้เงื่อนไขบอก 3 Browser นับเป็น Case เดียว แต่มีไว้เพื่อความเป็นมาตรฐาน */
   projects: [
     {
       name: 'chromium',
